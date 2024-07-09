a19125d9354e2ce3f1e7a5a9375abc1b8f8036b8
MariaC-Projects <mcuadra2024@fau.edu>
2024-07-09 13:58:13 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+function calcWordFrequencies(words) {
+    let wordsArray = words.split(" ");
+    let wordMap = new Map();
+    for(let word of wordsArray){
+      if(wordMap.has(word)){
+         wordMap.set(word,wordMap.get(word)+1);
+      }else{
+         wordMap.set(word,1);
+      }
+    }
+    for(let[word,frequency] of wordMap){
+      console.log(word + " "+frequency);
+    }
+
+}
+
+console.log("Testing calcWordFrequencies()...");
+calcWordFrequencies("hey hi Mark hi mark");
+
+
+
+// Do NOT remove the following line:
+export default calcWordFrequencies;
